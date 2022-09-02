ac00ae303527a33aaaea677af1882e5bafb414f5
Kevin <kmgoldfarb@gmail.com>
2022-09-02 15:34:03 -0400

    Removed unused var from ElectionCard.

--- src/components/Layout/ElectionCard.js
+++ src/components/Layout/ElectionCard.js
@@ -1,6 +1,6 @@
 const ElectionCard = props => {
   console.log(props.data);
-  const { election, pollingLocations, contests } = props.data;
+  const { election, pollingLocations } = props.data;
   if (pollingLocations.length > 0) {
     function makeStringAddress(location) {
       return `${location['address'].locationName} ${location['address'].line1} ${location['address'].city} ${location['address'].state} ${location['address'].zip}`;
